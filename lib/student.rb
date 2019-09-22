f0fcbfcd125a6e539caaeccec51327ba9998b4ae
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-09-22 19:29:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,8 +42,8 @@ class Student
  
   end
   
-   def self.create(name, grade)
-    student = Student.new(name, grade, id = nil)
+  def self.create(name, grade)
+    student = Student.new(name, grade)
     student.save
     student
   end
